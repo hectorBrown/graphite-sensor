f2160ec8514ba0ea2f0665317c60a936954261e1
Hector Brown <hectorjbrown@protonmail.com>
2026-04-02 16:40:13 +1100

    notes from today

--- analysis.py
+++ analysis.py
@@ -2,14 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-data = np.loadtxt("./readings/sensor1-idle.txt")
+signal, time = np.loadtxt("./readings/sensor1-idle_2.txt", delimiter=",", unpack=True)
+dc_signal, dc_time = np.loadtxt(
+    "./readings/sensor1-idle_3.txt", delimiter=",", unpack=True
+)
+time = (time - time[0]) / 1000
+dc_time = (dc_time - dc_time[0]) / 1000
 
-plt.hist(data)
-# plt.show()
+plt.xlabel("ADC Output")
+plt.ylabel("Number of readings")
+plt.hist(signal, bins=100)
+plt.savefig("./obsidian/graphite-sensor/figures/sensor1-idle-hist.png")
+plt.show()
+print(np.std(signal))
+print(np.mean(signal))
+print(np.mean(signal) / np.std(signal))
+average = np.mean(signal)
+dc_average = np.mean(dc_signal)
 
-t = np.arange(0, 1.0, 1 / len(data))
-fft_vals = np.fft.rfft(data)
-fft_freqs = np.fft.rfftfreq(len(data), 1 / len(data))
+fft_vals = np.fft.rfft(signal - average)
+dc_fft_vals = np.fft.rfft(dc_signal - dc_average)
+fft_freqs = np.fft.rfftfreq(len(signal), time[1] - time[0])
+dc_fft_freqs = np.fft.rfftfreq(len(dc_signal), dc_time[1] - dc_time[0])
 
-plt.plot(fft_freqs[1:], fft_vals[1:])
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Power")
+plt.plot(fft_freqs, np.abs(fft_vals) ** 2, label="Laptop plugged in")
+plt.plot(dc_fft_freqs, np.abs(dc_fft_vals) ** 2, label="Laptop unplugged")
+plt.legend()
+plt.savefig("./obsidian/graphite-sensor/figures/laptop-groundloop-demo.png")
 plt.show()
--- graphite-sensor.ino
+++ graphite-sensor.ino
@@ -1,16 +1,21 @@
-const int adcPin = 34;
+#include<cstdio>
 
+const int adcPin = 34;
+const int time_step = 5;
+int ms_time = 0;
 
 void setup() {
   Serial.begin(9600);
   analogSetAttenuation(ADC_11db);   
   analogSetWidth(12);
-
 }
 
 void loop() {
+
   float rawValue = analogRead(adcPin);
-  float voltage = 3.3f * (rawValue / 4095);
-  Serial.println(voltage);
-  delay(10);
+  Serial.print(rawValue);
+  Serial.print(",");
+  Serial.println(ms_time);
+  delay(time_step);
+  ms_time += time_step;
 }
